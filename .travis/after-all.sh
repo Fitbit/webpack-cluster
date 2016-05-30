86f0df61e01efa186dc5182e35556cfbdcbcc1da
Marat Dreizin <marat.dreizin@gmail.com>
2016-05-30 19:07:28 +0300

    chore(travis): adds missing `build` and `codeclimate` tasks

--- .travis/after-all.sh
+++ .travis/after-all.sh
@@ -3,6 +3,8 @@ declare exitCode;
 $(npm bin)/travis-after-all
 exitCode=$?
 if [ $exitCode -eq 0 ]; then
+  npm run build;
+  npm run codeclimate;
   npm run semantic-release;
   npm run jsdoc;
   npm run gh-pages;
