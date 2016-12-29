c61c5797936d862b5167ff2f69c6e5456d64de63
Marat Dreizin <marat.dreizin@gmail.com>
2016-12-29 12:27:17 +0300

    test(closeWatchers): fixes `watcher.close` issue

--- test/helpers/closeWatchers.js
+++ test/helpers/closeWatchers.js
@@ -4,6 +4,7 @@
  */
 export default watchers => {
     if (Array.isArray(watchers)) {
-        watchers.forEach(watcher => watcher.close());
+        watchers.filter(watcher => typeof watcher.close === 'function')
+            .forEach(watcher => watcher.close());
     }
 };
